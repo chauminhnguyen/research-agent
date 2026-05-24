01ce77bd44fcd5624103a4e23f13708a46437cf4
chauminhnguyen <minhchauss123456@gmail.com>
2026-05-24 11:35:46 +0700

    fix backend

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-};
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "font-src 'self' https://vercel.com https://*.vercel.com https://*.gstatic.com https://*.public.blob.vercel-storage.com;"
+          }
+        ],
+      },
+    ]
+  },
+}
 
-module.exports = nextConfig;
+export default nextConfig;
